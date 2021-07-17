44747307e46947f1e067de728b0ab493d848de57
Rodrigo Petrizzo <petrizzo.rodrigoa@gmail.com>
2021-07-17 02:54:29 -0300

    [Base]

--- encription_methods.py
+++ encription_methods.py
@@ -1,6 +1,14 @@
+from termcolor import colored, cprint
 
-def get_positions(master_password):
+def get_positions(master_password, alphabet=''):
+    """
+    Get the new positions of the alphabet
+
+    Returns:
+        new_alphabet : str 
+    """
     first_move = len(master_password)//2
+
     vocals_quantity = {
                         'a':0,
                         'e':0,
@@ -8,6 +16,7 @@ def get_positions(master_password):
                         'o':0,
                         'u':0
                         }
+    
     for i in master_password:
         if i == 'a':
             vocals_quantity['a'] = vocals_quantity['a'] + 1
@@ -18,29 +27,60 @@ def get_positions(master_password):
         elif i == 'o':
             vocals_quantity['o'] = vocals_quantity['o'] + 1
         elif i == 'u':
-            vocals_quantity['u'] = vocals_quantity['u'] + 1     
+            vocals_quantity['u'] = vocals_quantity['u'] + 1      
     
     finish_result = first_move + vocals_quantity['a'] - vocals_quantity['e'] - vocals_quantity['i'] + vocals_quantity['o'] - vocals_quantity['u']       
+    
     if finish_result > 26:
         finish_result = finish_result - 26
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    new_alphabet = alphabet[-finish_result:] + alphabet[:-finish_result]        
+    new_alphabet = alphabet[:finish_result] + alphabet[finish_result:]        
+    if alphabet == '':
+        alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        new_alphabet = alphabet[-finish_result:] + alphabet[:-finish_result]        
+        
+    print(finish_result)
     return new_alphabet
 
-def encrypter(master_password, password_to_encrypt):
-    new_alphabet = get_positions(master_password)
-    for index, i in enumerate(password_to_encrypt):
-        password_to_encrypt = password_to_encrypt.replace(i,new_alphabet[index])
-    new_alphabet = get_positions(password_to_encrypt)  
-    for index, i in enumerate(password_to_encrypt):
-        password_to_encrypt = password_to_encrypt.replace(i,new_alphabet[index])  
-    new_alphabet = get_positions(password_to_encrypt)  
-    for index, i in enumerate(password_to_encrypt):
-        password_to_encrypt = password_to_encrypt.replace(i,new_alphabet[index])        
-    print(password_to_encrypt)
-def decrypter(master_password, password_to_decrypt):
-    pass
-master_password = 'totest'
-password_to_encrypt = 'totest'
 
-encrypter(master_password, password_to_encrypt)
+def encrypter(master_password, password_to_encrypt):
+    """
+    Encrypt the given password
+    Args:
+        master_password : str not hashed
+        password_to_encrypt : str
+
+    Returns: 
+        encrypted_password : str    
+    """
+    new_alphabet = get_positions(master_password)
+    if len(new_alphabet) > len(password_to_encrypt):
+        new_alphabet = new_alphabet[0:len(password_to_encrypt)]
+
+    for index, i in enumerate(new_alphabet):
+        print(index, i)
+        print(password_to_encrypt[index])
+        password_to_encrypt = password_to_encrypt.replace(password_to_encrypt[index],new_alphabet[index],1)
+    print(new_alphabet, password_to_encrypt)    
+    
+    return password_to_encrypt, new_alphabet
+def decrypter(master_password, password_to_decrypt, alphabet):
+    
+    new_alphabet = get_positions(master_password,alphabet)
+
+
+    
+    for index, i in enumerate(password_to_decrypt):
+        
+        password_to_decrypt = password_to_decrypt.replace(i,new_alphabet[index])
+    print(new_alphabet, password_to_decrypt)
+
+master_password = 'Holabuenastardes'
+password = 'estaesmipassword'
+last_pass = 'fgtufgxyzugggefg'
+last_alphabet = 'rstuvwxyzabcdefghijklmnopq'
+
+encrypter(master_password, password)
+#decrypter(master_password, last_pass,last_alphabet)
+"""if __name__ == '__main__':
+    print(colored("You shouldn't be running this file", 'red'))
+    exit()"""
--- password_manager.py
+++ password_manager.py
@@ -2,6 +2,31 @@
 import sqlite3
 from hashlib import sha256
 
+class Database():
+    """
+    """
+    def __init__(self):
+        self.conn = sqlite3.connect("password_manager.db")
+        self.cur = self.conn.cursor()
+    def create_structure(self):    
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS masterpassword (
+	                                                    id INT AUTO UNIQUE NOT NULL PRIMARY KEY,
+   	                                                    user CHAR NOT NULL,
+	                                                    hashedpass CHAR NOT NULL
+                                                            )""")
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS savedpasswords (
+	                                                    id INT AUTO UNIQUE NOT NULL PRIMARY KEY,
+   	                                                    place CHAR NOT NULL,
+	                                                    pass CHAR NOT NULL
+                                                            )""")
+    def upload_password(self,**kwargs):
+        table_to_insert = kwargs['table']
+        first_row = kwargs['first_row']
+        second_row = kwargs['second_row']
+        first_argument = kwargs['first_argument']
+        second_argument = kwargs['second_argument']
+        self.cur.execute(f"INSERT INTO {table_to_insert} (id,{first_row},{second_row}) VALUES (?,'{first_argument}','{second_argument}')")
+        self.conn.commit()
 class PasswordManager():
 
     def __init__(self):
@@ -27,4 +52,5 @@ class PasswordManager():
         """
         pass
 
-
+database = Database()
+database.upload_password(table='savedpasswords',first_row='place',second_row='pass',first_argument='facebook',second_argument='hola')
